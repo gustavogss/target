bc52b59a16f94dc1fa3f118de095babc6057ad9a
Gustavo Souza <gustavogss.dev@gmail.com>
2026-01-11 00:35:05 -0300

    feat: HomeHearder component created

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,16 @@
-import { fontFamily } from '@/theme/fontFamily';
-import { router } from 'expo-router';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { HomeHeader } from "@/components/HomeHeader";
+import { View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-        <Text style={{fontFamily: fontFamily.bold}}>Olá, Expo Route!</Text>        
-       <Button title="New Target" onPress={() => router.navigate('/target')}/>
-       <Button title="Transaction" onPress={() => router.navigate('/transaction/123')}/>
-        <Button title="InProgress" onPress={() => router.navigate('/in-progress/80')}/>
+        <HomeHeader />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 10,
+        flex: 1,       
     }
 })
